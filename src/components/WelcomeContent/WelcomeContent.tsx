7a88d0413d0e08e454e52428fe207a272c90794e
H-morii <haruki.mori21@gmail.com>
2024-04-11 19:59:06 +0900

    suggested driver slider completed

--- src/components/WelcomeContent/WelcomeContent.tsx
+++ src/components/WelcomeContent/WelcomeContent.tsx
@@ -1,5 +1,62 @@
 
 import { CiDeliveryTruck } from "react-icons/ci";
+import DefaultProfile from "../../assets/defaultProfilePicture.jpg"
+import suggestedDrivers from "../../seedData/recomendedDriver.js";
+
+//slider
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+// Custom arrow components
+function SampleNextArrow(props:any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+    className={className}
+    style={{...style,
+      display: 'flex', // Use flexbox to center content
+      justifyContent: 'center', // Center horizontally
+      alignItems: 'center', // Center vertically
+      background: 'grey',
+      borderRadius: '50%', // Set the width of the circle
+      zIndex: 1
+     }} // Make sure it's above other elements
+    onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props:any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+    className={className}
+    style={{       ...style,
+      display: 'flex', // Use flexbox to center content
+      justifyContent: 'center', // Center horizontally
+      alignItems: 'center', // Center vertically
+      background: 'grey',
+      borderRadius: '50%', // Set the width of the circle
+      zIndex: 1,
+
+     }}
+    onClick={onClick}
+    />
+  );
+}
+
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
 
 
 const WelcomeContent: React.FC = () => {
@@ -27,27 +84,48 @@ const WelcomeContent: React.FC = () => {
       //   </div>
         <>
         <div className="">
-          <header className="flex w-full justify-center items-center bg-black text-white h-[100px]">
+          <div className="flex justify-center">
+          <header className="flex w-full justify-center items-center bg-black text-white h-[100px] md:w-[300px] md:my-4 md:rounded-2xl">
             <h2 className=" text-2xl">
               Welcome Back, Jessa Tot.
             </h2>
           </header>
+          </div>
           <div className="flex justify-center items-center">
             <div className="p-5">
               <h3 className="p-5 flex justify-center text-2xl"><CiDeliveryTruck size={"30"}/>Find driver</h3>
-            <input type="text" placeholder="Driver or location" className="border-2 w-[300px] p-2" />
+            <input type="text" placeholder="Driver or location" className="border-2 w-[300px] p-2 rounded-md" />
             <div className="pt-5 flex justify-center">
               <button className=" border py-3 px-6 hover:bg-black hover:text-white rounded-2xl">Find</button>
             </div>
             </div>
           </div>
-          <section>
-            <div className="p-4">
+            <div className="flex justify-center items-center p-4">
               <h2 className="text-2xl">Suggestion</h2>
             </div>
-
-          </section>
-        </div>
+            <div className="flex justify-center">
+            <div className="w-[300px]">
+            <Slider {...settings} >
+              {suggestedDrivers.map((driver) => (
+                <div key={driver.id} className=" m-4 shadow-sm rounded-lg p-2 ">
+                  <div className="">
+                  <figure>
+                    <img src={DefaultProfile} alt="Profile Picture" className=" rounded-t-lg" />
+                  </figure>
+                  <div className=" text-xs">
+                    <p>{driver.name}</p>
+                    <p>{driver.location}</p>
+                  </div>
+                  </div>
+                  </div>
+              ))} 
+              </Slider>
+              </div>
+              <div>
+              </div>
+              </div>
+            </div>
+           
         </>
   )
 }
---
+++ src/seedData/recomendedDriver.js
@@ -0,0 +1,59 @@
+const suggestedDrivers = [
+    {
+        id: 1,
+        name: "Kazuki Tanaka",
+        location: "Tokyo, Shibuya",
+        rating: 4.8,
+        vehicle: "Toyota Proace",
+        languages: ["Japanese", "English"],
+        experience: "5 years",
+        image: "driver-1.jpg", // Path to the driver's image
+        reviewsCount: 124,
+    },
+    {
+        id: 2,
+        name: "Reina Inoue",
+        location: "Osaka, Kita",
+        rating: 4.6,
+        vehicle: "Nissan NV200",
+        languages: ["Japanese"],
+        experience: "3 years",
+        image: "driver-2.jpg", // Path to the driver's image
+        reviewsCount: 98,
+    },
+    {
+        id: 3,
+        name: "Takumi Sato",
+        location: "Kyoto, Nakagyo",
+        rating: 4.9,
+        vehicle: "Honda Acty",
+        languages: ["Japanese", "English", "Spanish"],
+        experience: "8 years",
+        image: "driver-3.jpg", // Path to the driver's image
+        reviewsCount: 152,
+    },
+    {
+        id: 4,
+        name: "Yui Aragaki",
+        location: "Fukuoka, Hakata",
+        rating: 4.7,
+        vehicle: "Suzuki Carry",
+        languages: ["Japanese"],
+        experience: "6 years",
+        image: "driver-4.jpg", // Path to the driver's image
+        reviewsCount: 136,
+    },
+    {
+        id: 5,
+        name: "Ryuji Iwasaki",
+        location: "Sapporo, Chuo",
+        rating: 4.5,
+        vehicle: "Mitsubishi Minicab",
+        languages: ["Japanese", "English"],
+        experience: "4 years",
+        image: "driver-5.jpg", // Path to the driver's image
+        reviewsCount: 87,
+    },
+];
+
+export default suggestedDrivers;
